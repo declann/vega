a651ff36cd3f0897054aa1b236f82e701db62432
Declan Naughton <declan.naughton2@mail.dcu.ie>
2021-12-19 18:12:36 +0000

    add pre_traversal_id

--- packages/vega-hierarchy/src/lookup.js
+++ packages/vega-hierarchy/src/lookup.js
@@ -1,8 +1,10 @@
 // Build lookup table mapping tuple keys to tree node instances
 export default function(tree, key, filter) {
   const map = {};
-  tree.each(node => {
+  var pre_traversal_id = -1;
+  tree.eachBefore(node => {
     const t = node.data;
+    node.data.pre_traversal_id = ++pre_traversal_id;
     if (filter(t)) map[key(t)] = node;
   });
   tree.lookup = map;
